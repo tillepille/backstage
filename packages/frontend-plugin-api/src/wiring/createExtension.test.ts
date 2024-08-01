ab5920cba984cb26a69c0fe9adfca7d9b1e75d90
blam <ben@blam.sh>
2024-08-01 12:06:16 +0200

    chore: test with the extension tester
    Signed-off-by: blam <ben@blam.sh>

--- packages/frontend-plugin-api/src/wiring/createExtension.test.ts
+++ packages/frontend-plugin-api/src/wiring/createExtension.test.ts
@@ -596,7 +596,7 @@ describe('createExtension', () => {
       const overriden = testExtension.override({
         config: {
           schema: {
-            bar: z => z.string().optional(),
+            bar: z => z.string().default('hello'),
           },
         },
         factory(originalFactory, { config }) {
@@ -608,7 +608,15 @@ describe('createExtension', () => {
         },
       });
 
-      const tester = createExtensionTester(overriden).render();
+      expect(createExtensionTester(overriden).data(stringDataRef)).toBe(
+        'foo-boom-override-hello',
+      );
+
+      expect(
+        createExtensionTester(overriden, {
+          config: { foo: 'hello', bar: 'world' },
+        }).data(stringDataRef),
+      ).toBe('foo-hello-override-world');
     });
   });
 });
